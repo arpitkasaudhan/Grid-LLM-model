78a5d66fcc2891c4736fb41735d7c9d4d5277510
arpitkasaudhan <kasaudhanarpit37@gmail.com>
2023-08-21 04:13:41 +0530

    First commit:

--- constants.py
+++ constants.py
@@ -1,8 +1,3 @@
-import os 
-import chromadb
-from chromadb.config import Settings 
-CHROMA_SETTINGS = Settings(
-        chroma_db_impl='duckdb+parquet',
-        persist_directory='db',
-        anonymized_telemetry=False
-)
+version https://git-lfs.github.com/spec/v1
+oid sha256:739957d6ef98429864d4c6d27ccbcb3d895ffd857319235cd21be27458ae212f
+size 209
--- ingest.py
+++ ingest.py
@@ -1,31 +1,3 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader, PDFMinerLoader 
-from langchain.text_splitter import RecursiveCharacterTextSplitter 
-from langchain.embeddings import SentenceTransformerEmbeddings 
-from langchain.vectorstores import Chroma 
-import os 
-from constants import CHROMA_SETTINGS
-
-persist_directory = "db"
-def main():
-    for root, dirs, files in os.walk("docs"):
-        for file in files:
-            if file.endswith(".pdf"):
-                print(file)
-                loader = PDFMinerLoader(os.path.join(root, file))
-    documents = loader.load()   
-    print("splitting into chunks")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-    texts = text_splitter.split_documents(documents)
-    #create embeddings here
-    print("Loading sentence transformers model")
-    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    #create vector store here
-    print("Creating embeddings. May take some minutes...")
-    db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
-    db.persist()
-    db=None 
-
-    print("Ingestion complete! You can now run privateGPT.py to query your documents")
-
-if __name__ == "__main__":
-    main()
+version https://git-lfs.github.com/spec/v1
+oid sha256:0788fe9b33aa0a54ae56601521397faf6d0560c9841fe744c302c9894b8e05f2
+size 1309
